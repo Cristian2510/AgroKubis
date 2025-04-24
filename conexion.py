97c6c7c130bb69b00c108c501fd2d930263d8ab3
Cristian Garay <cristian2581@gmail.com>
2025-04-24 16:44:14 -0300

    Versión compatible con Windows y Render

--- conexion.py
+++ conexion.py
@@ -1,15 +1,19 @@
 import fdb
+import os
+import platform
 
-# Configuración de la base de datos (servidor público)
+# Configuración de la base de datos
 dsn = '170.82.145.121/3050:C:/Sistema Gol/Database/DATABASE.FDB'
 user = 'SYSDBA'
 password = 'di20071987'
 charset = 'NONE'
 
-def obtener_cdcs_por_fecha(desde, hasta):
-    # ❌ Elimina fdb.load_api, no funciona en Render
-    # fdb.load_api(fb_library_name)
+# Solo cargar la DLL en Windows
+if platform.system() == 'Windows':
+    fb_library_name = r'C:\Program Files\Firebird\Firebird_3_0\bin\fbclient.dll'
+    fdb.load_api(fb_library_name)
 
+def obtener_cdcs_por_fecha(desde, hasta):
     con = fdb.connect(
         dsn=dsn,
         user=user,
@@ -30,5 +34,4 @@ def obtener_cdcs_por_fecha(desde, hasta):
     resultados = cur.fetchall()
 
     con.close()
-
     return resultados
